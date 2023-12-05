341ac54bdb94d19e3a17877cbf277bd6a32f7c02
CTC8901228 <114205447+CTC8901228@users.noreply.github.com>
2023-12-05 18:23:00 +0800

    Add files via upload
    加入o3d可視化  簡易acc2tra

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 import random
 import matplotlib.pyplot as plt
 from icp import icp
-HOST = '192.168.162.246' # IP address 
+import open3d as o3d
+HOST = '192.168.221.246' # IP address 
 PORT = 42342 # Port to listen on (use ports > 1023)
 
 def main():
@@ -19,28 +20,44 @@ def main():
                 with conn:
                     print('Connected by', addr)
                     acclist=[]
-
+                    gyrlist=[]
+                    time_list=[]
                     while True:
-                        data = conn.recv(1024).decode('utf-8')
+                        data = conn.recv(1024*8).decode('utf-8')
                         print(data)
 
                         try:
-                            arr=list(map(int,data.split()))
+                            arr=list(map(int,data.split()[0:7]))
+                            float_arr=list(map(float,data.split()))
+                            
+                     
                         except:
                             print('data split prob, data=',data)
                             continue
-                        acc=data['acc']
-                        mode=data['mode']
+                        mode = arr[0]
+                        acc=[arr[1],arr[2],arr[3]-1024]
+                        gyr=[float_arr[7],float_arr[8],float_arr[9]]
+                        t=arr[-1]
+                        
                         if mode==0:
                             if(sending_pos==True):
-                                  tra=acc2tra(acclist)
-                                  pos_ind,err=POS_ID.Pos_identification(tra)
+                                    tra=acc2tra(acclist,time_list)
+                                    print(tra)
+                                    point_cloud = o3d.geometry.PointCloud()
+                                    point_cloud.points = o3d.utility.Vector3dVector(tra)
+                                    o3d.visualization.draw_geometries([point_cloud])
+                                    
+                                    pos_ind,err=POS_ID.Pos_identification(tra)
+                                    sending_pos=False
                             acclist=[]
                             continue  #idle
                         elif mode == 1:
-                             acclist.append(acc)
-                             #手勢辨識,輸入加速度 放加速度進list 在idle時判斷
-                             pass
+                            sending_pos=True
+                            acclist.append(acc)
+                            time_list.append(t)
+                            gyrlist.append(gyr)
+                            #手勢辨識,輸入加速度 放加速度進list 在idle時判斷
+                            pass
                         elif mode ==2 :
                         #滑鼠模式 輸入加速度
                             pass
@@ -65,6 +82,32 @@ class POS_ID:
     def getPosName(self,i):
          return self.pos_name[i]
 
+def acc2tra(acc,time):
+    acc=np.array(acc)
+    time_diff=[]
+    for i,t in enumerate(time):
+        if i==0:
+            continue
+        else:
+            time_diff.append((time[i]-time[i-1])/1000)
+    time_diff.append(0)
+    time_diff=np.stack((np.array(time_diff),np.array(time_diff),np.array(time_diff)),axis=-1)
+    seqlen=acc.shape[0]
+    add=acc*time_diff
+    vlist=[add[0,::]]
+    for i in range(1,seqlen):
+        
+        vlist.append(vlist[-1]+add[i,::])
+    
+    varr = np.array(vlist)
 
-                        
+    add=varr*time_diff
+    xlist=[add[0,::]]
+    for i in range(1,seqlen-1):
+        
+        xlist.append(xlist[-1]+add[i,::])
+    
+    return np.array(xlist)
+if __name__== '__main__':
+    main()
                         
